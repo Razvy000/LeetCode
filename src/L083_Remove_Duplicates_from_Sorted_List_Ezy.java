58e35d709a2b09335f4a365bd20145360b21b926
Razvy000 <razvy000@gmail.com>
2015-05-13 22:07:54 +0300

    first

--- src/L083_Remove_Duplicates_from_Sorted_List_Ezy.java
+++ src/L083_Remove_Duplicates_from_Sorted_List_Ezy.java
@@ -1,12 +1,12 @@
 
 /*
-Given a sorted linked list, delete all duplicates such that each element appear only once.
+ Given a sorted linked list, delete all duplicates such that each element appear only once.
 
-For example,
-Given 1->1->2, return 1->2.
-Given 1->1->2->3->3, return 1->2->3.
+ For example,
+ Given 1->1->2, return 1->2.
+ Given 1->1->2->3->3, return 1->2->3.
 
-*/
+ */
 public class L083_Remove_Duplicates_from_Sorted_List_Ezy {
 
     // idea: two pointers
@@ -20,12 +20,10 @@ public class L083_Remove_Duplicates_from_Sorted_List_Ezy {
             while (q != null) {
                 if (q.next != null && p.val == q.next.val) {
                     q.next = q.next.next;
-                }else{
+                } else {
                     q = q.next;
                 }
-                
             }
-
             p = p.next;
         }
 
