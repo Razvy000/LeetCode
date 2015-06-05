87133fd7eba135ee94ec67c477b5dee541d5a754
Razvy000 <razvy000@gmail.com>
2015-06-05 11:16:36 +0300

    first

--- src/L037_Sudoku_Solver_Hrd.java
+++ src/L037_Sudoku_Solver_Hrd.java
@@ -1,4 +1,11 @@
 
+/*
+ Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+ Empty cells are indicated by the character '.'.
+
+ You may assume that there will be only one unique solution.
+ */
 public class L037_Sudoku_Solver_Hrd {
 
     // idea: straight forward implementation using backtrack
