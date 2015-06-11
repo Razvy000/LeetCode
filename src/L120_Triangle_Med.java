53c08a5e9b58670734e52fb34b4264eb5ed86121
Razvy000 <razvy000@gmail.com>
2015-06-11 10:21:50 +0300

    first

---
+++ src/L120_Triangle_Med.java
@@ -0,0 +1,42 @@
+
+import java.util.List;
+/*
+Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+
+For example, given the following triangle
+[
+     [2],
+    [3,4],
+   [6,5,7],
+  [4,1,8,3]
+]
+The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+
+Note:
+Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
+*/
+
+public class L120_Triangle_Med {
+
+    // idee bottom up
+    // bottom[]
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int h = triangle.size();
+        int n = triangle.get(h - 1).size();
+        int[] bottom = new int[n];
+
+        // initialize bottom
+        for (int i = 0; i < n; i++) {
+            bottom[i] = triangle.get(h - 1).get(i);
+        }
+
+        // loop
+        for (int i = h - 2; i >= 0; i--) {
+            List<Integer> line = triangle.get(i);
+            for (int j = 0; j < line.size(); j++)
+                bottom[j] = Math.min(bottom[j], bottom[j + 1]) + line.get(j);
+        }
+
+        return bottom[0];
+    }
+}
