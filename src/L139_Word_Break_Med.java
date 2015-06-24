1f2fa813cce240bce7b492fc351035738b3e9db9
Razvy000 <razvy000@gmail.com>
2015-06-24 14:00:34 +0300

    first

--- src/L139_Word_Break_Med.java
+++ src/L139_Word_Break_Med.java
@@ -3,15 +3,14 @@ import java.util.Set;
 
 
 /*
-Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+ Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
 
-For example, given
-s = "leetcode",
-dict = ["leet", "code"].
+ For example, given
+ s = "leetcode",
+ dict = ["leet", "code"].
 
-Return true because "leetcode" can be segmented as "leet code".
-*/
-
+ Return true because "leetcode" can be segmented as "leet code".
+ */
 public class L139_Word_Break_Med {
 
     // idea: dynamic programming
