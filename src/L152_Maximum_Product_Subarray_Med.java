f98d7a6addfc06c4de627365dc995ce42c3e8df6
Razvy000 <razvy000@gmail.com>
2015-06-30 09:44:14 +0300

    first

---
+++ src/L152_Maximum_Product_Subarray_Med.java
@@ -0,0 +1,49 @@
+
+import java.util.List;
+/*
+ Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+ For example, given the array [2,3,-2,4],
+ the contiguous subarray [2,3] has the largest product = 6.
+ */
+
+public class L152_Maximum_Product_Subarray_Med {
+
+    // idea: dinamic programming
+    // max array
+    // min array (negative * negative => positive)
+    // M[i][j] = max starting at i of length j
+    // M[i][j] = max(M[i][j-1])
+    public int maxProduct(List<Integer> nums) {
+        int[] array = toIntArray(nums);
+        return maxProduct(array);
+    }
+
+    public int maxProduct(int[] A) {
+        if (A.length == 0) {
+            return 0;
+        }
+
+        int maxherepre = A[0];
+        int minherepre = A[0];
+        int maxsofar = A[0];
+        int maxhere, minhere;
+
+        for (int i = 1; i < A.length; i++) {
+            maxhere = Math.max(Math.max(maxherepre * A[i], minherepre * A[i]), A[i]);
+            minhere = Math.min(Math.min(maxherepre * A[i], minherepre * A[i]), A[i]);
+            maxsofar = Math.max(maxhere, maxsofar);
+            maxherepre = maxhere;
+            minherepre = minhere;
+        }
+        return maxsofar;
+    }
+
+    int[] toIntArray(List<Integer> list) {
+        int[] ret = new int[list.size()];
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = list.get(i);
+        }
+        return ret;
+    }
+}
