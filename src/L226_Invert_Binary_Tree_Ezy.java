2e3d65714cc14724bbf3faacdb52378eb74abab2
Razvy000 <razvy000@gmail.com>
2015-12-15 21:51:32 +0000

    first

--- src/L226_Invert_Binary_Tree_Ezy.java
+++ src/L226_Invert_Binary_Tree_Ezy.java
@@ -16,6 +16,16 @@ Trivia:
 This problem was inspired by this original tweet by Max Howell:
 Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so fuck off.
  */
+
 public class L226_Invert_Binary_Tree_Ezy {
-    
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+
+        TreeNode left = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(left);
+        return root;
+    }
 }
